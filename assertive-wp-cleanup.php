bb4c85c653b9f8ab98e539b83ff1cef5ad662635
elicohenator <elicohenator@gmail.com>
2021-11-10 17:52:49 +0000

    initial

---
+++ assertive-wp-cleanup.php
@@ -0,0 +1,120 @@
+<?php
+/**
+* Plugin Name: Assertive WP Cleanup
+* Description: This plugin will remove comments, emojis, enforce better login errors, clean wp head and lots of other stuff. use at your own will
+* Version: 1.0
+* Author: Eli Cohen
+* Author URI: https://www.elicohenator.xyz
+**/
+
+
+/**
+ * Disable the emoji's
+ */
+function cleanup_disable_emojis()
+{
+  remove_action('wp_head', 'print_emoji_detection_script', 7);
+  remove_action('admin_print_scripts', 'print_emoji_detection_script');
+  remove_action('wp_print_styles', 'print_emoji_styles');
+  remove_action('admin_print_styles', 'print_emoji_styles');
+  remove_filter('the_content_feed', 'wp_staticize_emoji');
+  remove_filter('comment_text_rss', 'wp_staticize_emoji');
+  remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
+
+  // Remove from TinyMCE
+  add_filter('tiny_mce_plugins', 'disable_emojis_tinymce');
+}
+add_action('init', 'cleanup_disable_emojis');
+
+// return generic login error message
+function login_error_override()
+{
+    return 'Incorrect login details.';
+}
+add_filter('login_errors', 'login_error_override');
+
+/**
+ * Filter out the tinymce emoji plugin.
+ */
+function disable_emojis_tinymce($plugins)
+{
+  if (is_array($plugins)) {
+    return array_diff($plugins, array('wpemoji'));
+  } else {
+    return array();
+  }
+}
+
+/**
+ * Clean WP version
+ */
+function cleanup_remove_version()
+{
+  return '';
+}
+add_filter('the_generator', 'cleanup_remove_version');
+
+
+/**
+ * Remove junk from wp_head
+ */
+remove_action('wp_head', 'rsd_link');
+remove_action('wp_head', 'wp_generator');
+remove_action('wp_head', 'feed_links', 2);
+remove_action('wp_head', 'index_rel_link');
+remove_action('wp_head', 'wlwmanifest_link');
+remove_action('wp_head', 'feed_links_extra', 3);
+remove_action('wp_head', 'start_post_rel_link', 10, 0);
+remove_action('wp_head', 'parent_post_rel_link', 10, 0);
+remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0);
+remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
+remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
+remove_action('wp_head', 'wp_oembed_add_discovery_links', 10);
+remove_action('wp_head', 'rest_output_link_wp_head', 10);
+remove_action('template_redirect', 'rest_output_link_header', 11, 0);
+
+/* Disable WordPress Admin Bar for all users */
+add_filter( 'show_admin_bar', '__return_false' );
+
+/**
+ * Disbale comments.
+ */
+add_action('admin_init', function () {
+  // Redirect any user trying to access comments page
+  global $pagenow;
+
+  if ($pagenow === 'edit-comments.php') {
+    wp_redirect(admin_url());
+    exit;
+  }
+
+  // Remove comments metabox from dashboard
+  remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal');
+
+  // Disable support for comments and trackbacks in post types
+  foreach (get_post_types() as $post_type) {
+    if (post_type_supports($post_type, 'comments')) {
+      remove_post_type_support($post_type, 'comments');
+      remove_post_type_support($post_type, 'trackbacks');
+    }
+  }
+});
+
+// Close comments on the front-end
+add_filter('comments_open', '__return_false', 20, 2);
+add_filter('pings_open', '__return_false', 20, 2);
+
+// Hide existing comments
+add_filter('comments_array', '__return_empty_array', 10, 2);
+
+// Remove comments page in menu
+add_action('admin_menu', function () {
+  remove_menu_page('edit-comments.php');
+});
+
+// Remove comments links from admin bar
+add_action('init', function () {
+  if (is_admin_bar_showing()) {
+    remove_action('admin_bar_menu', 'wp_admin_bar_comments_menu', 60);
+  }
+});
